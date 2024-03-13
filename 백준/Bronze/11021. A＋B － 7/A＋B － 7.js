ee3d98f6630ff49ff254c9ff3c4271b3835ecfaf
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-13 15:58:53 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,6 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+for (let i = 1; i <= parseInt(input[0]); i++){
+    const [a, b] = input[i].split(" ").map(Number);
+    console.log(`Case #${i}: ${a+b}`);
+}
