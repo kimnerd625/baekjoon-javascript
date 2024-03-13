a90c89c6f2e8ff56e7e66970e26cd10905c869de
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-13 16:09:55 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -5,3 +5,16 @@ while (input.length !== 1) {
     const [a, b] = line.split(" ").map(Number);
     console.log(a + b);
 }
+
+/*
+모범 답안
+
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+while (input.length > 0) {
+    const line = input.shift(); // 첫 번째 요소를 빼내어 line에 저장
+    if (line === "0 0") break; // "0 0"을 만나면 반복 종료
+    const [num1, num2] = line.split(" ").map(Number);
+    console.log(num1 + num2);
+}
+*/
