c28d2a816c7001559833224de51203bacd06e283
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-14 15:49:24 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,27 +1,19 @@
-const readline = require("readline");
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const croatianAlphabet = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
 
-rl.question("", function(input) {
-    const croatianAlphabet = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
+let count = 0;
+let index = 0;
 
-    let count = 0;
-    let index = 0;
-
-    while (index < input.length) {
-        if (croatianAlphabet.includes(input.substring(index, index + 2))) {
-            index += 2;
-        } else if (croatianAlphabet.includes(input.substring(index, index + 3))) {
-            index += 3;
-        } else {
-            index += 1;
-        }
-        count++;
+while (index !== input.length) {
+    if (croatianAlphabet.includes(input.substring(index, index + 2))) {
+        index += 2;
+    } else if (croatianAlphabet.includes(input.substring(index, index + 3))) {
+        index += 3;
+    } else {
+        index += 1;
     }
+    count ++;
+}
 
-    console.log(count);
-    rl.close();
-});
+console.log(count);
