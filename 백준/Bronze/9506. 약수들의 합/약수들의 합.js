9a0d006f48f8ea222888099aaa73495b9536ac36
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-16 10:57:27 +0900

    [Bronze I] Title: 약수들의 합, Time: 148 ms, Memory: 10304 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.js
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.js
@@ -1,21 +1,26 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
 
-input.pop(); // 마지막 -1을 제거합니다.
-
-input.forEach(t => {
-    let factorArr = [];
-    let sum = 0;
+while (input.length !==0) {
+    const t = input.shift();
     
-    for (let i = 1; i < t; i++) { // t를 제외한 모든 약수 찾기
-        if (t % i === 0) {
+    if (t === -1) {
+        break;
+    }
+        
+    let factorArr = [];
+    
+    for (let i = 1; i < t; i++) {
+        if ( t % i === 0) {
             factorArr.push(i);
-            sum += i; // 약수의 합 계산
         }
     }
     
-    if (t === sum) { // 완전수 판별
+    const sum = factorArr.reduce((a, b) => a + b, 0);
+    
+    if (t === sum) {
         console.log(`${t} = ${factorArr.join(" + ")}`);
     } else {
         console.log(`${t} is NOT perfect.`);
     }
-});
+    
+}
