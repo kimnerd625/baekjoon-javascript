839a49e1e2d70f65dbb5d6b58854adcef1d63abc
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-16 10:05:27 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -23,6 +23,28 @@ while (snail < v) {
 
 */
 
+/*
 let dayCount = Math.ceil((v - b) / (a - b));
 
 console.log(dayCount);
+*/
+
+/*
+마지막의 경우를 생각했을 때,
+i) V에 도달을 A에 딱 맞게 했을 때, ii) 딱 맞게 하지 못했을 때,
+
+i)의 경우, 
+V - (A = 마지막 오르기)를 (A - B)로 나누면, 딱 나눠 떨어져야 함 (나머지 = 0)
+
+ii)의 경우,
+V - (A = 마지막 오르기)를 (A - B)로 나누면, 남는 게 있음 (찰나로 한 번에 못감.. 멍청한 달팽이)
+고로 하루를 더 써야 함
+*/
+
+let dayCount;
+
+if ((v - a) % (a - b) === 0 ) {
+    console.log( (v - a) / (a - b) + 1);
+} else {
+    console.log( parseInt((v - a) / (a - b)) + 2);
+}
