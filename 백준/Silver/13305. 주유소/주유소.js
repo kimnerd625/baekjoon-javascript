9021a94bcfbf40b7cedc3d04cb0907eb0a646bfe
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2024-03-28 14:31:40 +0900

    [Silver III] Title: 주유소, Time: 424 ms, Memory: 45060 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,17 +1,17 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const n = parseInt(input[0]);
-const dist = input[1].split(" ").map(Number);
-const price = input[2].split(" ").map(Number);
+const dist = input[1].split(" ").map(el => BigInt(el));
+const price = input[2].split(" ").map(el => BigInt(el));
 
-let answer = 0;
+let answer = 0n; // BigInt 타입으로 초기화
 let min_price = price[0];
 
 for (let i = 0; i < n - 1; i++) {
     if (price[i] < min_price) {
         min_price = price[i];
     }
-    answer += min_price * dist[i];
+    answer += min_price * dist[i]; // 여기서의 연산도 모두 BigInt 연산
 }
 
-console.log(answer);
+console.log(answer.toString()); // BigInt를 문자열로 변환하여 출력
